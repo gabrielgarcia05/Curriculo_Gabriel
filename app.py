4355d0f9d373dec6c8cf2e4931a1d00475756ef3
Gabriel Garcia <100931438+gabrielgarcia05@users.noreply.github.com>
2022-10-17 21:00:21 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from cProfile import run
+from socket import if_nameindex
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
